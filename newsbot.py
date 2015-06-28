3d56b2f6b52f858a077fe6495d97cf80eb72b8f8
Karan Sharma <karansharma1295@gmail.com>
2015-06-29 01:50:20 +0530

    Added praw

--- newsbot.py
+++ newsbot.py
@@ -2,13 +2,14 @@ import requests
 import logging
 import json
 import os
+import praw
 
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger('nbt')
 last_updated = 0
 BOT_KEY = os.environ['NBT_ACCESS_TOKEN']
 API_BASE = 'https://api.telegram.org/bot'
-
+r = praw.Reddit(user_agent='Telegram Xiled Chippians Group')
 
 def get_updates():
     log.debug('Fetching telegram request')
@@ -17,7 +18,11 @@ def get_updates():
 
 def get_latest_news():
     log.debug('fetching latest news')
-    return 'bloop bloop'
+    allposts=[]
+    submissions = r.get_subreddit('programming').get_top(limit=5)      #Can change the subreddit or add more.
+    for post in submissions:
+        allposts.append(post.permalink)                                #Permalink or actual URL ? 
+    return allposts
 
 
 def post_message(chat_id, text):
@@ -33,7 +38,8 @@ if __name__ == '__main__':
         log.debug('Fetched sucessfully')
         for req in r['result']:
             if last_updated < req['update_id']:
-                n = get_latest_news()
-                post_message(req['message']['chat']['id'], n)
+                n = get_latest_news()                                    #returns a list
+                for i in n:
+                    post_message(req['message']['chat']['id'], i)
         last_updated = req['update_id']
         log.debug('last_updated: {0}'.format(last_updated))
